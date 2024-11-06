eefda24f7ccc239db93307a9a0d91237abddf202
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-11-06 18:54:28 +0100

    a

---
+++ src/main/java/com/example/shopscanner_spring_api/ShopScannerSpringApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.shopscanner_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopScannerSpringApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ShopScannerSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shopscanner_spring_api/ShopScannerSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopscanner_spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopScannerSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
