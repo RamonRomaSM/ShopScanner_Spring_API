c322608a7a677050c7787671358a7ea0da788733
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-11-23 10:59:08 +0100

    Update User.java

--- src/main/java/com/example/shopscanner_spring_api/api/model/User.java
+++ src/main/java/com/example/shopscanner_spring_api/api/model/User.java
@@ -1,4 +1,16 @@
 package com.example.shopscanner_spring_api.api.model;
 
 public class User {
+    private String userId;
+    private String userName;
+    private String password;
+    private String email;//TODO: desarrollar la funcionalidad del email
+
+    public User(String userId, String userName, String password, String email) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
 }
