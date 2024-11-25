8dcaac2b6b60532c4e7132402b7ea75153b9c350
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-11-26 00:52:55 +0100

    Terminado servicio de producto

--- src/main/java/com/example/shopscanner_spring_api/api/controller/ProductController.java
+++ src/main/java/com/example/shopscanner_spring_api/api/controller/ProductController.java
@@ -1,4 +1,31 @@
 package com.example.shopscanner_spring_api.api.controller;
 
+import com.example.shopscanner_spring_api.api.model.Product;
+import com.example.shopscanner_spring_api.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
 public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/getPagina")
+    public ArrayList<Product> getPagina(@RequestParam int page, @RequestParam int size, @RequestParam(required = false, defaultValue = "%") String search, @RequestParam(required = false, defaultValue = "idProducto") String sort) {
+        return productService.getPage(page, size, search, sort);
+    }
+
+    @GetMapping("/getPoduct")
+    public Product getPoduct(@RequestParam String id) {
+        return productService.getProduct(id);
+    }
+
 }
--- src/main/java/com/example/shopscanner_spring_api/service/ProductService.java
+++ src/main/java/com/example/shopscanner_spring_api/service/ProductService.java
@@ -1,8 +1,21 @@
 package com.example.shopscanner_spring_api.service;
 
+import com.example.shopscanner_spring_api.api.model.Product;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    public Product getProduct(String idProducto) {
+        return null;
+    }
+
+    public ArrayList<Product> getPage(int page, int size, String searc, String sort) {
+        return null;
+    }
+
 
 }
