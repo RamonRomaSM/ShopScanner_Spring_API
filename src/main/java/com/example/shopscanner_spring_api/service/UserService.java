a80495d82f37bc1506cd349052925b381e3dd587
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-12-01 14:24:52 +0100

    a

--- src/main/java/com/example/shopscanner_spring_api/service/UserService.java
+++ src/main/java/com/example/shopscanner_spring_api/service/UserService.java
@@ -17,30 +17,33 @@ public class UserService extends GenericService{
     public User getUser(String nombre, String passw){
         try {
             //First we ask for the basic user info
-            String sql="SELECT \"ID\", \"NAME\", \"MAIL\" FROM USERS WHERE NAME = ? AND PASSW = ?";
+            String sql="SELECT \"id\", \"name\", \"mail\" FROM \"ShopScanner_Schema\".users WHERE name = ? AND passw = ?";
             PreparedStatement ps = this.connection.prepareStatement(sql);
             ps.setString(1, nombre);
             ps.setString(2, passw);
             ResultSet rs = ps.executeQuery();
 
-            String userId = rs.getString("ID");
-            String name = rs.getString("NAME");
-            String email = rs.getString("MAIL");
+            if (!rs.next()){return new User(null,null,null,null,null);}//In case user does not exist
+
+            String userId = rs.getString("id");
+            String name = rs.getString("name");
+            String email = rs.getString("mail");
 
             ps.close();
             rs.close();
 
             //Now we ask for the Shopping lists
             ArrayList<ShoppingList> shoppingLists = new ArrayList<>();
-            String sql2 = "SELECT * FROM SHOPPING_LISTS WHERE USER_ID = ?";
+            String sql2 = "SELECT * FROM \"ShopScanner_Schema\".shopping_lists WHERE user_id = ?";
             ps = this.connection.prepareStatement(sql2);
+            ps.setInt(1, Integer.parseInt(userId));
             rs = ps.executeQuery();
             while (rs.next()) {
 
-                String listId = rs.getString("LIST_ID");
-                String listName = rs.getString("NAME");
-                String lastModified = rs.getString("LAST_MODIFIED");
-                double totalPrice = rs.getDouble("TOTAL_PRICE");
+                String listId = rs.getString("list_id");
+                String listName = rs.getString("name");
+                String lastModified = rs.getString("last_modified");
+                double totalPrice = rs.getDouble("total_price");
                 ShoppingList act = new ShoppingList(listId,userId,listName,null,totalPrice,lastModified);
 
                 shoppingLists.add(act);
@@ -56,16 +59,16 @@ public class UserService extends GenericService{
 
     public boolean registerUser(String nombre, String passw, String email){
         try {
-            String sql = "INSERT INTO USERS (NAME, PASSW, MAIL) VALUES (?, ?, ?)";
+            String sql = "INSERT INTO \"ShopScanner_Schema\".users (name, passw, mail) VALUES (?, ?, ?)";
             PreparedStatement ps = this.connection.prepareStatement(sql);
             ps.setString(1,nombre);
             ps.setString(2,passw);
             ps.setString(3,email);
 
-            boolean resp = ps.execute();
+            ps.execute();
             ps.close();
 
-            return resp;
+            return true;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
